4ff37af0a8875bbbd721cb41ca8c5566a622f90a
jetmaquiling <jet.maquiling@gmail.com>
2021-11-16 12:35:17 +0800

    Dynamic PopUp

--- components/caveat/popup.js
+++ components/caveat/popup.js
@@ -7,21 +7,42 @@ import Head from 'next/head'
 import Carousel from 'nuka-carousel';
 import style from '@/styles/caveats/popup.module.css'
 import ClearIcon from '@material-ui/icons/Clear';
-
+import config from '@/config/configuration.json'
+import axios from 'axios';
 
 export default function Popup() {
-    const [open, setOpen] = React.useState(true)
+    const [data, setData] = React.useState({popup_image: null , popup_link: "" })
+    const [open, setOpen] = React.useState(false)
     const toggle = () =>{
         setOpen(!open)
     }
+
+   
+
+
+    React.useEffect(() => {
+        axios.get(`${config.SERVER_URL}/csr`).then(res => {
+            setData({popup_image: res.data.popup_image.url , popup_link: res.data.popup_link })
+        }).catch(error=> {
+          console.log(error)
+        })
+     }, [])
+    
+
+    React.useEffect(() => {
+        console.log("WORKING")
+        setTimeout(function(){ 
+            setOpen(true)
+        }, 5000);
+    },[])
 
   return (
             <div className={open ? style.main : style.off} onClick={()=>toggle()}>
                 <div className={style.xbutton} >
                     <ClearIcon style={{fontSize: "30px" , color: '#fff', cursor: 'pointer'}} />
                 </div>
-                <div className={style.imageContainer}>
-                   <img src="/Thumbnail/popup.jpg" alt="POP" className={style.image}/>
+                <div className={style.imageContainer}  >
+                       <Link href={data.popup_link}><img src={data.popup_image} alt="POP" className={style.image} /></Link>
                 </div>
             </div>
   );
